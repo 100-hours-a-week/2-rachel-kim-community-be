09f087de3191e81dd910fbf7d5dc1bd460363ccf
Myungbeem Kim <128389329+mbeankong@users.noreply.github.com>
2025-01-20 15:17:19 +0900

    http 보안헤더 추가

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import { fileURLToPath } from 'url';
+import helmet from 'helmet'; // 보안 헤더 추가 라이브러리
 
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
@@ -20,36 +21,70 @@ const app = express();
 const PORT = 4000;
 
 // 세션 설정
-app.use(session({
-    secret: process.env.SESSION_SECRET, // 임의의 비밀 키
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        secure: false, // HTTPS 사용 시 true
-        sameSite: 'lax', // 보다 간단한 세션 처리를 위해 'lax' 사용
-        maxAge: 1000 * 60 * 60 * 24, // 1일
-    },
-}));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET, // 임의의 비밀 키
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: false, // HTTPS 사용 시 true
+            sameSite: 'lax', // 로그인 인증에 필요한 세션 공유 허용
+            maxAge: 1000 * 60 * 60 * 24, // 1일
+        },
+    })
+);
 
 // CORS 설정
-app.use(cors({
-    origin: "http://localhost:3000", // 프론트엔드 주소
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: 'http://localhost:3000', // 클라이언트 도메인
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true, // 쿠키 허용
+    })
+);
+
+// Helmet 보안 헤더 설정
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'"],
+                styleSrc: ["'self'"],
+                imgSrc: ["'self'", "http://localhost:4000", "data:"],
+            },
+        },
+        referrerPolicy: {
+            policy: 'no-referrer', 
+        },
+        // X-Content-Type-Options 설정
+        noSniff: true
+    })
+);
 
 // JSON 데이터 처리를 위한 미들웨어 설정
 app.use(bodyParser.json());
 
 // 정적 파일 제공 설정
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(
+    '/public',
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true, // CORS 쿠키 허용
+    }),
+    (req, res, next) => {
+        res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+        next();
+    },
+    express.static(path.join(__dirname, 'public'))
+);
 
 // 사용자 관련 라우터
 app.use('/api/users', userRoutes); 
-
 // 게시글 관련 라우터
 app.use('/api/posts', postRoutes);
-
 // 댓글 관련 라우터 
 app.use('/api/posts/:post_id/comments', commentRoutes);
 
